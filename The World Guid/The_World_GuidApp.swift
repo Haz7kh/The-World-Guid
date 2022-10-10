662b1d1414160ee111ff5da9a652721c3082be6d
khaled Almelhem <90411108+Haz7kh@users.noreply.github.com>
2022-10-10 12:32:03 +0300

    Initial Commit

---
+++ The World Guid/The_World_GuidApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_World_GuidApp.swift
+//  The World Guid
+//
+//  Created by Khaled Almelhem on 2022-10-10.
+//
+
+import SwiftUI
+
+@main
+struct The_World_GuidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
